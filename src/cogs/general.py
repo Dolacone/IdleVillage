d2bdcd51bcd027bb2470c3c9b8c1fdbf296eb374
Dolacone <dolacone@gmail.com>
2026-04-06 15:49:33 +0800

    fly.io setup

--- src/cogs/general.py
+++ src/cogs/general.py
@@ -5,7 +5,7 @@ class General(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @disnake.slash_command(description="Ping the bot")
+    @commands.slash_command(description="Ping the bot")
     async def ping(self, inter: disnake.ApplicationCommandInteraction):
         await inter.response.send_message(f"Pong! Latency: {round(self.bot.latency * 1000)}ms")
 
